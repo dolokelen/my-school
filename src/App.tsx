7ada76618bc9c8c1a1696774fd25c87a36e0e13a
dolokelen <dolokelen1@gmail.com>
2023-10-06 00:41:53 +0800

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
-import MyButton from './components/Button'
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-
   return (
-    <MyButton />
-  )
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        sm: `"nav nav" "aside main"`,
+      }}
+      templateColumns={{
+        base: `1fr`,
+        sm: `200px 1fr`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="sm">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="green">
+        Main
+      </GridItem>
+    </Grid>
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -2,12 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { ChakraProvider } from '@chakra-ui/react'
+
+const client = new QueryClient()
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <ChakraProvider>
-      <App />
+      <QueryClientProvider client={client}>
+        <App />
+      </QueryClientProvider>
     </ChakraProvider>
   </React.StrictMode>,
 )
