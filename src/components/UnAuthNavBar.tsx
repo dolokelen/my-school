dabbccbdb8c97a361e9cb608894ecd63e95cbb82
dolokelen <dolokelen1@gmail.com>
2023-11-29 00:55:16 +0800

    Bug fixed- Fixed issue with login and logout btn labels

--- src/components/UnAuthNavBar.tsx
+++ src/components/UnAuthNavBar.tsx
@@ -5,13 +5,39 @@ import {
   HOME_ROUTE,
   LOGIN_ROUTE,
 } from "../cacheKeysAndRoutes";
+import getUserId from "../Utilities/getUserId";
+import logoutUser from "../Utilities/logoutUser";
+import { useEffect, useState } from "react";
 
 const UnAuthNavBar = () => {
+  const [userId, setUserId] = useState<number | undefined>();
+  const authUserId = getUserId();
+
+  useEffect(() => {
+    if (authUserId) {
+      setUserId(authUserId);
+    }
+  }, [authUserId]);
+
   return (
     <Flex justifyContent="space-evenly">
       <Link to={HOME_ROUTE}>Home</Link>
       <Link to={AUTH_LAYOUT_ROUTE}>Dashboard</Link>
-      <Link to={LOGIN_ROUTE}>Login</Link>
+      {/* The last "Login" route is used for the Dashboard "Logout" route.
+      This dynamic btn label is for users who might use the "Website" logout btn.*/}
+      {authUserId ? (
+        <Link
+          to={LOGIN_ROUTE}
+          onClick={() => {
+            logoutUser();
+            setUserId(undefined);
+          }}
+        >
+          {userId ? "Logout" : "Login"}
+        </Link>
+      ) : (
+        <Link to={LOGIN_ROUTE}>Login</Link>
+      )}
     </Flex>
   );
 };
